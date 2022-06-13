ce4d1f9b68dea543ede5d591f851953f87d499d6
Jonathan <jonathanmoorevla@gmail.com>
2022-06-13 17:01:39 -0500

    commit tree fcfabe3fd99265616bb942896760d006a85f2136

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// console.log(customerName);
+// console.log(upperCaseCustomerName());
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jim';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'ferdinand';
+}
